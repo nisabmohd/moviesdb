8c5e297e4ba2605094d84c281f05315743df33da
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2022-01-18 21:36:47 +0530

    Update Discovercard.jsx

--- src/components/Discovercard.jsx
+++ src/components/Discovercard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../css/discover.css'
+import { Link } from 'react-router-dom';
 export const Discovercard = (props) => {
     return (
         <div className='discovercard'>
-            <a href={"/details?id="+props.id+"&cat="+props.cat}><img src={`https://image.tmdb.org/t/p/original${props.img}`} alt="" /></a>
+            <Link to={"/details?id="+props.id+"&cat="+props.cat}><img src={`https://image.tmdb.org/t/p/original${props.img}`} alt="" /></Link>
             <h5>{props.title}</h5>
             <p>{props.date}</p>
         </div>
