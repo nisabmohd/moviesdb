bb7af4ddc0b5673802c2eb7eaa11c6847ccd95f0
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2022-01-18 21:34:16 +0530

    Update Cardc.jsx

--- src/components/Cardc.jsx
+++ src/components/Cardc.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../css/card.css'
+import { Link } from 'react-router-dom';
 export const Cardc = (props) => {
     return (
         <div className='card'>
-            <a href={"/details?id=" + props.id + "&cat=" + props.cat}><img src={`https://image.tmdb.org/t/p/original${props.image}`} alt="" /></a>
+            <Link to={"/details?id=" + props.id + "&cat=" + props.cat}><img src={`https://image.tmdb.org/t/p/original${props.image}`} alt="" /></Link>
             {/* <img src={`https://image.tmdb.org/t/p/original${props.image}`} alt="" /> */}
             <div className='ratings_circle'><p >{props.ratings * 10}%</p></div>
             <h5>{props.title}</h5>
